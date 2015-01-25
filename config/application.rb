5d2095faf02b9b261835b82b66e881005b409949
Mike <michukimyxx@gmail.com>
2015-01-26 00:48:26 +0200

    Adding Pending Specs

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,9 @@ module SmTestApp
 		:helper_specs => false, 
 		:routing_specs => false, 
 		:controller_specs => true, 
-		:request_specs => true 	 g.fixture_replacement :factory_girl, 
+		:request_specs => true 	 
+        
+        g.fixture_replacement :factory_girl, 
 		:dir => "spec/factories" 
 	end
   end
---
+++ spec/models/feedback_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper' 
+
+describe Feedback do 
+   it "has a valid factory" 
+   it "is invalid without text" 
+   it "returns a full string of entered feedback" 
+end
