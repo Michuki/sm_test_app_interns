75a61b3168f1e9413c4e650b734417e3b24be0ca
Mike <michukimyxx@gmail.com>
2015-01-26 07:21:32 +0200

    4.0 Tests with Capybara

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :test do
 	gem 'capybara' 
 	gem 'guard-rspec' 
 	gem 'launchy' 
+	gem 'database_cleaner'
 end
 
 
---
+++ spec/requests/feedbacks_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper' 
+# spec/requests/feedbacks_spec.rb
+
+describe "Feedbacks" do 
+	describe "Manage Feedback Listing" do 
+		it "Adds a new Feedback and displays the results" do 
+			visit feedbacks_url 
+			expect{ 
+				click_link 'New Feedback' 
+				fill_in 'feedback_text', with: "Final Job" 
+				click_button "Create Feedback" 
+			}.to change(Feedback,:count).by(1) 
+			page.should have_content "Feedback was successfully created."
+				within 'h1' do 
+				page.should have_content "Final Job" 
+				end 
+			end 
+
+		it "Deletes a feedback" do 
+			feedback = create(:feedback, text: "Final Job")
+			visit feedbacks_path 
+				expect{ 
+					within "#Feedback_#{feedback.id}" do 
+					click_link 'Destroy' 
+				end 
+				alert = page.driver.browser.switch_to.alert
+				alert.accept
+		}.to change(Feedback,:count).by(-1) 
+		page.should have_content "Listing feedbacks" 	
+		page.should_not have_content "Final Job" 
+	end
+     end 
+end
