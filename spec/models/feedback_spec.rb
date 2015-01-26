738599272fe51b7be6c6627b26d675c7e444b07a
Mike <michukimyxx@gmail.com>
2015-01-26 02:22:59 +0200

    2.0 First Valid Test

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper' 
 
 describe Feedback do 
-   it "has a valid factory" 
+   it "has a valid factory" do
+	course = FactoryGirl.create(:feedback).should be_valid
+   end
    it "is invalid without text" 
    it "returns a full string of entered feedback" 
 end
