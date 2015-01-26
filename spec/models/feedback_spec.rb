d60e4e6d2e84c63ecfcb3d2ef15d14822506df8a
Mike <michukimyxx@gmail.com>
2015-01-26 02:59:25 +0200

    2.2 Model Tests Complete

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper' 
 
 describe Feedback do 
-   it "has a valid factory" do
-	feedback = FactoryGirl.create(:feedback).should be_valid
+   it "has a valid factory class" do
+	create(:feedback).should be_valid
    end
    it "is invalid without text" do
 	build(:feedback, text: nil).should_not be_valid 
